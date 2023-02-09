96563ab8d08dfbf7f0b3a570ea28cd7295fab085
aalberty <46965141+aalberty@users.noreply.github.com>
2023-02-09 22:02:27 +0000

    header and footer folder structure and angular linking

--- app.js
+++ app.js
@@ -1,43 +1,6 @@
-var app = angular.module('mainApp', []);
+angular.module('mainApp', [])
 
-app.controller('mainCtrl', function ($scope) {
-    loadLib();
-
-    function loadLib () {
-        //Dirs containing html, controller, and css
-        let standardDirs = [
-            'header',
-            //'footer'
-        ];
-
-        standardDirs.forEach(function (dir) {
-            addStandardDir(dir);
-        });
-    }
-
-    function addStandardDir (dirName) {
-        let filePath = "lib/";
-        let fileTypesObj = {
-            html: {
-                nameSuffix: "",
-                extension: ".html"
-            },
-            style: {
-                nameSuffix: "",
-                extension: ".css"
-            },
-            controller: {
-                nameSuffix: "Ctrl",
-                extension: ".js"
-            }
-        };
-
-        for (fileType in fileTypesObj) {
-            filePath += dirName + "/" + dirName + fileType.nameSuffix + fileType.extension;
-            angular.element(document.getElementsByTagName('head'))
-            .append(
-                angular.element('<script src="' + filePath + '" />')
-            );
-        }
-    } 
+.controller('mainCtrl', function ($scope) {
+    $scope.greeting = "Hello, World!";
+    
 });
